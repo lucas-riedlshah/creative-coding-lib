b5d871a1ae9aa9c9fa54462adef367c9ee1ce949
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2023-03-29 23:38:45 +1300

    fixed index so it is a true "module"

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 import { Emitter } from './Emitter';
 import { Circle } from './Circle';
 import { Particle } from './Particle';
-export { Emitter, Circle, Particle };
+import { Vector2 } from './Vector2';
+export { Emitter, Circle, Particle, Vector2 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Emitter } from './Emitter'
 import { Circle } from './Circle'
 import { Particle } from './Particle'
+import { Vector2 } from './Vector2'
 
-export { Emitter, Circle, Particle }
+export { Emitter, Circle, Particle, Vector2 }
