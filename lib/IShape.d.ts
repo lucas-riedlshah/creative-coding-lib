f5cece58fcdc29158db049737a14094a65e82c88
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2023-04-05 01:03:11 +1200

    add position attribute to IShape

--- lib/IShape.d.ts
+++ lib/IShape.d.ts
@@ -1,6 +1,7 @@
 import { IPositionable } from "./IPositionable";
 import { Vector2 } from "./Vector2";
 export interface IShape extends IPositionable {
+    position: Vector2;
     get_random_point(random_func: () => number): Vector2;
     contains(point: Vector2): boolean;
 }
