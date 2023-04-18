90236097f30e51adeac2737e58a9b36c572baebf
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2023-04-18 15:16:45 +1200

    update brushes to use ICurve

--- lib/brushes.d.ts
+++ lib/brushes.d.ts
@@ -1,20 +1,16 @@
 import p5 from "p5";
 import { Vector2 } from "./Vector2";
-import { Line } from ".";
+import { ICurve } from "./ICurve";
 export declare function generate_pencil_brush(p5: p5, options?: {
     brush_diameter?: number;
     density?: number;
     diameter_variability?: number;
     noise_scale?: number;
     bristle_diameter?: number;
-    bristle_distribution?: {
-        use_gaussian: boolean;
-        gaussian_mean: number;
-        gaussian_sd: number;
-    };
-}): (line: Line, position: Vector2) => void;
+    bristle_radial_distribution_func?: () => number;
+}): (curve: ICurve, position: Vector2) => void;
 export declare function generate_ink_brush(p5: p5, options?: {
     min_diameter?: number;
     max_diameter?: number;
     blotchiness?: number;
-}): (line: Line, position: Vector2) => void;
+}): (curve: ICurve, position: Vector2) => void;
--- src/brushes.ts
+++ src/brushes.ts
@@ -1,6 +1,6 @@
 import p5 from "p5";
 import { Vector2 } from "./Vector2";
-import { Line } from ".";
+import { ICurve } from "./ICurve";
 
 export function generate_pencil_brush(
   p5: p5,
@@ -10,36 +10,27 @@ export function generate_pencil_brush(
     diameter_variability?: number,
     noise_scale?: number,
     bristle_diameter?: number,
-    bristle_distribution?: {
-      use_gaussian: boolean,
-      gaussian_mean: number,
-      gaussian_sd: number
-    }
+    bristle_radial_distribution_func?: () => number
   }
 ) {
-  const brush_diameter = options?.brush_diameter || 100
+  const brush_diameter = options?.brush_diameter || 2
   const density = options?.density || 3
   const diameter_variability = options?.diameter_variability ? Math.max(0, Math.min(1, options.diameter_variability)) : 1
   const bristle_diameter = options?.bristle_diameter || 1
   const noise_scale = options?.noise_scale || 0.05
-  const use_gaussian = options?.bristle_distribution?.use_gaussian || false
-  const gaussian_mean = options?.bristle_distribution?.gaussian_mean || 0
-  const gaussian_sd = options?.bristle_distribution?.gaussian_sd || 0.5
+  const bristle_radial_distribution_func = options?.bristle_radial_distribution_func || (() => Math.sqrt(p5.random()))
 
-  return (line: Line, position: Vector2) => {
-    const distance_from_start = Vector2.distance(line.start, position)
+  return (curve: ICurve, position: Vector2) => {
+    const distance_from_start = Vector2.distance(curve.start, position)
     const diameter =
       brush_diameter -
-      brush_diameter * diameter_variability * p5.noise(line.start.x, line.start.y, distance_from_start * noise_scale);
+      brush_diameter * diameter_variability * p5.noise(curve.start.x, curve.start.y, distance_from_start * noise_scale);
     const N = density * diameter;
     for (let i = 0; i < (N < 1 ? +(p5.random() < N) : N); i++) {
       const offsetted_position = Vector2.forward;
       offsetted_position.rotate(p5.random() * Math.PI * 2);
       offsetted_position.multiply_in_place(
-        Math.sqrt(use_gaussian 
-          ? Math.abs(p5.randomGaussian(gaussian_mean, gaussian_sd)) 
-          : p5.random())
-         * diameter * 0.5
+        bristle_radial_distribution_func() * diameter * 0.5
       );
       offsetted_position.add_in_place(position);
       p5.circle(offsetted_position.x, offsetted_position.y, bristle_diameter);
@@ -63,8 +54,8 @@ export function generate_ink_brush(
 
   const DIAMETER_DIFFERENCE = max_diameter - min_diameter;
 
-  return (line: Line, position: Vector2) => {
-    const distance_from_start = Vector2.distance(line.start, position)
+  return (curve: ICurve, position: Vector2) => {
+    const distance_from_start = Vector2.distance(curve.start, position)
     p5.circle(
       position.x,
       position.y,
